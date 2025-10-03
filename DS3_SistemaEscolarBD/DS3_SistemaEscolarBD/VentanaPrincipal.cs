4fe6be0e524058f747eb0cc37b4a1590270e81ac
damianenriquez680 <a224204026@unison.mx>
2025-10-03 09:37:41 -0700

    Organizacion Actividad
    Primer Commit

---
+++ DS3_SistemaEscolarBD/DS3_SistemaEscolarBD/VentanaPrincipal.cs
@@ -0,0 +1,10 @@
+namespace DS3_SistemaEscolarBD
+{
+    public partial class VentanaPrincipal : Form
+    {
+        public VentanaPrincipal()
+        {
+            InitializeComponent();
+        }
+    }
+}
